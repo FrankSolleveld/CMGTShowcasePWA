b48fb677c434c1da03e7cc7960227cfdf01d7630
IAmMisterFrank <fsolleveld@outlook.com>
2020-03-23 12:00:40 +0100

    First commit, added empty sw

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+/*
+Service Worker
+created by: Frank Solleveld
+*/
+
+self.addEventListener('install', (event) => {
+    console.log('[Service Worker] Installing service worker.', event)
+})
+
+self.addEventListener('activate', (event) => {
+    console.log('[Service Worker] Activating service worker.', event)
+    return self.clients.claim()
+})
